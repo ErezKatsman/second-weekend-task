ff1b787a41e0c5d95ee9b11b331ee41b6d647049
ErezKatsman <erezkatsman2311@gmail.com>
2021-01-24 17:07:06 +0200

    finished task

--- tableCr.js
+++ tableCr.js
@@ -123,44 +123,75 @@ for (const workout of workouts) {
   );
 }
 
+const title = document.createElement("h1");
+title.innerText = "My Workouts !";
+title.className = "title";
+document.body.append(title);
+const table = document.createElement("table");
+document.body.append(table);
+const thead = table.createTHead();
+let tr = thead.insertRow();
+
 // I made the first row of the table
-document.write(
-  `<h1 class="title">My Workouts !</h1>
-  <table class='mainTable'><tr>
-  <th>Type</th>
-  <th>Start</th>
-  <th>End</th>
-  <th>Total<br>Hours</th>
-  <th>Goals<br>Given</th>
-  <th>Goals<br>Archived</th>
-  <th>Goals<br>Archived %</th>
-  
-  </tr>`
-);
+const headers = [
+  "Type",
+  "Start",
+  "End",
+  "Total Hours",
+  "Given",
+  "Archived",
+  "Archived %",
+];
+
+for (const header of headers) {
+  const th = document.createElement("th");
+  th.innerText = header;
+  tr.append(th);
+}
 
 // I entered all the rows (with for(of)) in my table and painted the hours and % with if statements and named classes in td tags
+const tbody = table.createTBody();
+
 for (const workout of workouts) {
-  document.write(
-    `<tr>
-    <td>${workout.workoutType}</td>
-    <td>${workout.startedAt.getHours()}:00</td>
-    <td>${workout.finishedAt.getHours()}:00</td>
-    <td class=${
-      workout.totalTime >= 5
-        ? "goodTime"
-        : workout.totalTime >= 3
-        ? "mediumTime"
-        : "badTime"
-    }>${workout.totalTime}</td>
-    <td>${workout.goalsGiven}</td>
-    <td>${workout.goalsArchived}</td>
-    <td class=${
-      workout.archivedPercent >= 80
-        ? "goodArchive"
-        : workout.archivedPercent >= 50
-        ? "mediunArchive"
-        : "smallArchive"
-    }>${workout.archivedPercent}%</td>
-    </tr>`
-  );
+  let tr = tbody.insertRow();
+
+  let tdType = document.createElement("td");
+  tdType.innerText = workout.workoutType;
+  tr.append(tdType);
+
+  let tdStart = document.createElement("td");
+  tdStart.innerText = workout.startedAt.getHours() + ":00";
+  tr.append(tdStart);
+
+  let tdEnd = document.createElement("td");
+  tdEnd.innerText = workout.finishedAt.getHours() + ":00";
+  tr.append(tdEnd);
+
+  let tdTotal = document.createElement("td");
+  tdTotal.innerText = workout.totalTime + ":00";
+  tr.append(tdTotal);
+  tdTotal.className =
+    workout.totalTime >= 5
+      ? "goodTime"
+      : workout.totalTime >= 3
+      ? "mediumTime"
+      : "badTime";
+
+  let tdGiven = document.createElement("td");
+  tdGiven.innerText = workout.goalsGiven;
+  tr.append(tdGiven);
+
+  let tdArchived = document.createElement("td");
+  tdArchived.innerText = workout.goalsArchived;
+  tr.append(tdArchived);
+
+  let tdPer = document.createElement("td");
+  tdPer.innerText = workout.archivedPercent + "%";
+  tr.append(tdPer);
+  tdPer.className =
+    workout.archivedPercent >= 80
+      ? "goodArchive"
+      : workout.archivedPercent >= 50
+      ? "mediumArchive"
+      : "smallArchive";
 }
